78f8d6b3afe75be9238a1423a7d0754221c10e6d
jantimmer91 <jan.timmer@hotmail.de>
2021-12-18 14:56:06 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Uebersicht from "./views/Uebersicht";
+
+
+function App() {
+
+    return (
+        <div>
+            <Uebersicht/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/views/Uebersicht.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const ContentContainer = styled.div`
+
+`;
+
+class Uebersicht extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    componentDidMount() {
+        document.body.style.backgroundColor = "rgba(203,203,210,.15)"
+    }
+
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+Uebersicht.propTypes = {};
+
+export default Uebersicht;
